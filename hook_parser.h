7658fe82df7689bbb528b91a1f4f55a078caec18
Florian Ragwitz <rafl@debian.org>
2008-10-21 13:12:13 +0200

    Make the setup function available for c code.

--- hook_parser.h
+++ hook_parser.h
@@ -3,6 +3,7 @@
 
 #include "perl.h"
 
+void hook_parser_setup (void);
 const char *hook_parser_get_linestr (pTHX);
 IV hook_parser_get_linestr_offset (pTHX);
 void hook_parser_set_linestr (pTHX_ const char *new_value);
