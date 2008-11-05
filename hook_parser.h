e447153c787758c819a0d139b1abeb0e10abe402
Florian Ragwitz <rafl@debian.org>
2008-11-05 12:24:11 +0100

    Declare all public functions in the header file.

--- hook_parser.h
+++ hook_parser.h
@@ -5,6 +5,7 @@
 #include "hook_op_check.h"
 
 hook_op_check_id hook_parser_setup (void);
+void hook_parser_teardown (hook_op_check_id id);
 char *hook_parser_get_linestr (pTHX);
 IV hook_parser_get_linestr_offset (pTHX);
 void hook_parser_set_linestr (pTHX_ const char *new_value);
@@ -13,5 +14,6 @@ char *hook_parser_get_lex_stuff (pTHX);
 void hook_parser_clear_lex_stuff (pTHX);
 char *hook_toke_skipspace (pTHX_ char *s);
 char *hook_toke_scan_str (pTHX_ char *s);
+char *hook_toke_scan_word (pTHX_ int offset, int handle_package, char *dest, STRLEN destlen, STRLEN *res_len);
 
 #endif
